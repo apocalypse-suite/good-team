c18916af7b8edfb35cd42e49da7c8bfaa735c690
apocalypse-suite <mitchenko.catherine@gmail.com>
2020-10-16 15:10:07 +0300

    created routing layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+/*  {
+    path: '',
+    component:
+  },
+  {
+    path: 'users',
+    component
+  },
+  {
+  path: 'todos',
+    component:
+  },
+  {
+    path: 'logOut',
+    component:
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
