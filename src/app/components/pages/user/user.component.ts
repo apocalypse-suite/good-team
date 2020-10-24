0422b207032a54409d26fc4efb211786a38d13f9
Kristina Sorochan <khristina.sorochan@gmail.com>
2020-10-24 19:23:55 +0300

    add users page

--- src/app/components/pages/user/user.component.ts
+++ src/app/components/pages/user/user.component.ts
@@ -7,6 +7,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  users = [
+    {
+      imageUrl: 'assets/images/icon-02.png',
+      user: {
+        name: 'Minnie Ferguson',
+        location: 'Chesapeake, USA'
+      },
+      skills: ['Angular JS', 'React', '.NET'],
+      price: '$150/hour',
+      hour: '32 Hour/week',
+      score: '365543'
+    },
+    {
+      imageUrl: 'assets/images/icon-01.png',
+      user: {
+        name: 'Caleb Castillo',
+        location: 'Philadelphia, Bangladesh'
+      },
+      skills: ['Angular JS', 'React', '.NET'],
+      price: '$200/hour',
+      hour: '21 Hour/week',
+      score: '10543'
+    },
+    {
+      imageUrl: 'assets/images/icon-03.png',
+      user: {
+        name: 'Ann Hunter',
+        location: 'Lexington-Fayette, India'
+      },
+      skills: ['Angular JS', 'React', '.NET'],
+      price: '$90/hour',
+      hour: '25 Hour/week',
+      score: '234543'
+    },
+    {
+      imageUrl: 'assets/images/icon-04.png',
+      user: {
+        name: 'Florence Harmon',
+        location: 'Carpus Christi, France'
+      },
+      skills: ['Angular JS', 'React', '.NET'],
+      price: '$240/hour',
+      hour: '24 Hour/week',
+      score: '54321'
+    },
+    {
+      imageUrl: 'assets/images/icon-02.png',
+      user: {
+        name: 'Minnie Ferguson',
+        location: 'Chesapeake, USA'
+      },
+      skills: ['Angular JS', 'React', '.NET'],
+      price: '$150/hour',
+      hour: '32 Hour/week',
+      score: '365543'
+    },
+    {
+      imageUrl: 'assets/images/icon-01.png',
+      user: {
+        name: 'Caleb Castillo',
+        location: 'Philadelphia, Bangladesh'
+      },
+      skills: ['Angular JS', 'React', '.NET'],
+      price: '$200/hour',
+      hour: '21 Hour/week',
+      score: '10543'
+    },
+    {
+      imageUrl: 'assets/images/icon-03.png',
+      user: {
+        name: 'Ann Hunter',
+        location: 'Lexington-Fayette, India'
+      },
+      skills: ['Angular JS', 'React', '.NET'],
+      price: '$90/hour',
+      hour: '25 Hour/week',
+      score: '234543'
+    },
+    {
+      imageUrl: 'assets/images/icon-04.png',
+      user: {
+        name: 'Florence Harmon',
+        location: 'Carpus Christi, France'
+      },
+      skills: ['Angular JS', 'React', '.NET'],
+      price: '$240/hour',
+      hour: '24 Hour/week',
+      score: '54321'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
